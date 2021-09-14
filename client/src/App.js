9c67c8f3b788157207405f5b3e1e0e6db691b034
leofer1995 <u2902486@unimilitar.edu.co>
2021-09-14 15:54:55 -0500

    cambio en app.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
       
       <Route path='/' component={NavBar}/>{/* Leo: Barra de Navegacion  */}
-      <Route exact path= '/home'  component={Home}/>{/*Leo: Componente Home*/}
+      <Route exact path= '/'  component={Home}/>{/*Leo: Componente Home*/}
 
       <Route path='/formUser' component={FormUsers} />
 
