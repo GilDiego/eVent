99e88d25f1c3e9b0c8ccd6b8ea3219754ca1811e
Cesar Caviedes <cesca2092@gmail.com>
2021-09-14 19:58:00 -0500

    New back config

--- server/routes/rutaprueba.js
+++ server/routes/rutaprueba.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const testcontroller = require('../controllers/testcontroller');
+const testcontroller2 = require('../controllers/testcontroller2');
 
-router.get('/',
+
+router.get('/prueba',
     testcontroller.controller
+)
+
+router.get('/prueba3',
+    testcontroller.controller3
+)
+
+router.get('/prueba2',
+    testcontroller2.testcontroller2
 )
 
 module.exports = router;
