c7e685497f566d08b90cb0353acf63a7f7d396f9
Diego Gil <diegogilhz@gmail.com>
2021-09-14 19:54:19 -0500

    adds Event Details for Usuario, missing styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import {BrowserRouter, Route } from 'react-router-dom';
-import FormUsers from './components/formUsers';
+import { BrowserRouter, Route } from 'react-router-dom';
+// import FormUsers from './components/formUsers';
+import EventDetailsUsario from './components/Details/EventDetailsUsario/EventDetailsUsario';
 
 function App() {
   return (
     <BrowserRouter>
-    <Route path='/' component={FormUsers} />
+      <Route path='/' component={EventDetailsUsario} />
     </BrowserRouter>
   );
 }
